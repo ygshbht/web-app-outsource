ce7caa9caf045951a7ed1c91b2736f0da2aa25ef
Yogesh Bhtt <65986357+ygshbht@users.noreply.github.com>
2022-06-13 16:10:10 +0530

    Latest (#5)
    * Add Burger and Side menu
    
    * Clear warnings
    
    * Use CSS modules
    
    * Add .prettierrc and remove package copy .json
    
    * Convert 4 spaces indentation to 2 spaces
    
    * Convert hypen css  names to camelCase
    
    * Transition from bootstrap classes to CSS ones

--- src/web/components/Menus/MenuDiscover/MenuDiscover.js
+++ src/web/components/Menus/MenuDiscover/MenuDiscover.js
@@ -1,30 +1,14 @@
 import React, { useEffect, useState, useContext } from "react";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import mapboxgl from "web/MapboxGlWrapper";
-import {
-  Tooltip,
-  OverlayTrigger,
-  Container,
-  Row,
-  Col,
-  Button,
-} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { MapContext } from "web/MapProvider";
 // import "./MenuDiscover.css";
 import styles from "./MenuDiscover.module.css";
 
 import Form from "react-bootstrap/Form";
-import {
-  InfoCircle,
-  Water,
-  Building,
-  CaretUpFill,
-  ArrowBarLeft,
-  Eye,
-  Search,
-  CurrencyDollar,
-  Bullseye,
-} from "react-bootstrap-icons";
+import { ArrowBarLeft, Eye, Search, Bullseye } from "react-bootstrap-icons";
+import TilesInfo from "web/components/TilesInfo/TilesInfo";
 
 const MenuDiscover = (_) => {
   const { map } = useContext(MapContext);
@@ -78,18 +62,13 @@ const MenuDiscover = (_) => {
       <Row>
         <Col>
           {expanded ? (
-            <div
-              className="d-flex justify-content-between"
-              style={{
-                gap: "15px",
-              }}
-            >
-              <div className="d-flex">
-                <label className="form-label text-white">Lng:</label>
+            <div className={styles.latLongContainer}>
+              <div>
+                <label>Lng:</label>
                 <Form.Control type="text" placeholder="Search"></Form.Control>
               </div>
-              <div className="d-flex">
-                <label className="form-label text-white">Lat:</label>
+              <div>
+                <label>Lat:</label>
                 <Form.Control type="text" placeholder="Search"></Form.Control>
               </div>
             </div>
@@ -133,13 +112,7 @@ const MenuDiscover = (_) => {
         {expanded ? (
           <TilesInfo />
         ) : (
-          <div
-            className="font-weight-bold text-white text-uppercase"
-            style={{
-              marginTop: 10,
-              writingMode: "vertical-rl",
-            }}
-          >
+          <div className={styles.tilesSummaryCondensed}>
             Tiles selected: &nbsp; 0/1000
           </div>
         )}
@@ -148,226 +121,11 @@ const MenuDiscover = (_) => {
   );
 };
 
-function AppRow({ children }) {
-  return (
-    <div className="d-flex align-items-start" style={{ gap: 10 }}>
-      <div
-        className="d-flex justify-content-center pt-1"
-        style={{ minWidth: "7%" }}
-      >
-        {children[0]}
-      </div>
-      <div style={{ flexGrow: "1" }}>{children[1]}</div>
-    </div>
-  );
-}
-
 function IconButton({ icon: Icon, onClick }) {
   return (
     <div onClick={onClick} className={styles.iconButton}>
       <Icon></Icon>
     </div>
-  );
-}
-
-function TilesInfo() {
-  return (
-    <Col className={styles.tilesInfo} style={{ color: "white" }}>
-      <div style={{ color: "white" }}>
-        <div
-          className="text-text-uppercase d-flex justify-content-between text-white"
-          style={{
-            marginTop: "15px",
-          }}
-        >
-          <div className="font-weight-bold">Tiles Selected: -/1000</div>
-          <a
-            className="text-white"
-            style={{ textDecoration: "underline" }}
-            href="/"
-          >
-            Clear selection
-          </a>
-        </div>
-      </div>
-      <div>
-        <div
-          className="uppercase font-weight-bold"
-          style={{
-            margin: "10px 0",
-            color: "rgb(44, 206, 143)",
-          }}
-        >
-          No tiles selected
-        </div>
-      </div>
-      <div>
-        <div className={styles.tilesInfoCardTop}>
-          <div className="font-weight-bold d-flex align-items-center justify-content-between">
-            <p className="m-0" style={{ fontSize: "1.5rem" }}>
-              {" "}
-              Budavári Palota, Budapest
-            </p>
-            <Bullseye />
-          </div>
-          <div className={styles.areaGeographyInfo}>
-            <div>
-              <Building />
-              <span>644</span>
-            </div>
-            <div>
-              <CaretUpFill />
-              <span>644</span>
-            </div>
-            <div>
-              <Water />
-              <span>0</span>
-            </div>
-          </div>
-          <div className="d-flex align-items-center  text-uppercase">
-            <span
-              style={{
-                color: "orange",
-                border: "1px solid orange",
-                padding: "2px 5px",
-                fontSize: "0.8rem",
-              }}
-            >
-              Not land art compatible
-            </span>
-            <OverlayTrigger
-              placement="top"
-              overlay={
-                <Tooltip>
-                  Neque porro quisquam est qui dolorem ipsum quia dolor sit
-                  amet, consectetur, adipisci velit..." "There is no one who
-                  loves pain itself, who seeks after it and wants to have it,
-                  simply because it is pain...
-                </Tooltip>
-              }
-            >
-              <InfoCircle style={{ marginLeft: 10 }} />
-            </OverlayTrigger>
-          </div>
-          <AppRow>
-            <div>
-              <CurrencyDollar style={{ fontSize: "1.5rem" }} />
-            </div>
-            <div>
-              <div className="d-flex font-font-weight-bold align-items-center">
-                <span style={{ fontSize: "1.5rem" }}>
-                  <span>Total</span>
-
-                  <span className="text-primary" style={{}}>
-                    &nbsp;64.4 USDT
-                  </span>
-                </span>
-                <OverlayTrigger
-                  placement="top"
-                  overlay={
-                    <Tooltip>
-                      Neque porro quisquam est qui dolorem ipsum quia dolor sit
-                      amet, consectetur, adipisci velit..." "There is no one who
-                      loves pain itself, who seeks after it and wants to have
-                      it, simply because it is pain...
-                    </Tooltip>
-                  }
-                >
-                  <InfoCircle style={{ marginLeft: 5 }} />
-                </OverlayTrigger>
-              </div>
-              <div className="d-flex">
-                <div className="p-0 md-col-4 col-6 text-secondary">
-                  {" "}
-                  Price per tile
-                </div>
-                <div className="col-8"> 0.1 USDT</div>
-              </div>
-            </div>
-          </AppRow>
-          <AppRow>
-            <InfoCircle />
-
-            <div className="d-flex flex-column ">
-              <div className="d-flex">
-                <div className="p-0 md-col-4 col-6 text-secondary">
-                  {" "}
-                  Current owner:
-                </div>
-
-                <div className="col-8"> zsef</div>
-              </div>
-              <div className="d-flex">
-                <div className="p-0 md-col-4 col-6 text-secondary">
-                  {" "}
-                  Purchased for:
-                </div>
-                <div className="col-8"> 66.4 USDT</div>
-              </div>
-
-              <div className="d-flex">
-                <div className="md-col-4 col-6 p-0 text-secondary">
-                  {" "}
-                  Price per tile
-                </div>
-                <div className="col-8"> 0.1 USDT</div>
-              </div>
-            </div>
-          </AppRow>
-
-          <AppRow>
-            <InfoCircle />
-            <div>
-              <div> Budapest, Szent Gyorgy Utca 2, 1013, Hungary</div>
-              <div className="text-secondary">19.03921 47.495895</div>
-            </div>
-          </AppRow>
-        </div>
-        <div
-          className={styles.tilesInfoCardBottom}
-          style={{ background: "#240051" }}
-        >
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div className="text-secondary p-0 col-6">Current market value</div>
-            <div className="p-0 col-3">721.79 USDT</div>
-            <span className="p-0 col-3">10,20 %</span>
-          </div>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div className=" text-secondary p-0 col-6">
-              Original new land value:
-            </div>
-            <div className="p-0 col-3">0.1 USDT</div>
-            <div className="p-0 col-3"></div>
-          </div>
-          <div className={styles.tilesSelectedButtons}>
-            <Button
-              style={{
-                background:
-                  "linear-gradient(160.71deg,rgba(81,238,144,.2) .39%,rgba(17,17,17,0) 87.04%),radial-gradient(100% 100% at 100% 0%,#6100FF 0%,rgba(17,17,17,0) 100%),#874BE8",
-              }}
-            >
-              History
-            </Button>
-            <Button
-              style={{
-                background:
-                  "radial-gradient(100% 683.38% at 100% -1.52%,#AA20EB 0%,#FF6B00 100%)",
-              }}
-            >
-              Inspect
-            </Button>
-            <Button
-              style={{
-                background:
-                  "radial-gradient(100% 683.38% at 100% -1.52%,#AA20EB 0%,#FF6B00 100%)",
-              }}
-            >
-              Place an offer
-            </Button>
-          </div>
-        </div>
-      </div>
-    </Col>
   );
 }
 
---
+++ src/web/components/TilesInfo/TilesInfo.js
@@ -0,0 +1,185 @@
+import React from "react";
+import { Tooltip, OverlayTrigger, Col, Button } from "react-bootstrap";
+// import "./MenuDiscover.css";
+import styles from "./TilesInfo.module.css";
+
+import {
+  InfoCircle,
+  Water,
+  Building,
+  CaretUpFill,
+  CurrencyDollar,
+  Bullseye,
+} from "react-bootstrap-icons";
+
+export default function TilesInfo() {
+  return (
+    <Col className={styles.tilesInfo}>
+      <div>
+        <div style={{ fontWeight: "bold" }}>Tiles Selected: -/1000</div>
+        <a href="/">Clear selection</a>
+      </div>
+
+      {/* <div>
+        <div
+          className="uppercase font-weight-bold"
+          style={{
+            margin: "10px 0",
+            color: "rgb(44, 206, 143)",
+          }}
+        >
+          No tiles selected
+        </div>
+      </div> */}
+      <div>
+        <div className={styles.tilesInfoCardTop}>
+          <div>
+            <p> Budavári Palota, Budapest</p>
+            <Bullseye />
+          </div>
+          <div className={styles.areaGeographyInfo}>
+            <div>
+              <Building />
+              <span>644</span>
+            </div>
+            <div>
+              <CaretUpFill />
+              <span>644</span>
+            </div>
+            <div>
+              <Water />
+              <span>0</span>
+            </div>
+          </div>
+          <div className={styles.landArtCompatibility}>
+            <span className={styles.compatibilityStatus}>
+              Not land art compatible
+            </span>
+            <OverlayTrigger
+              placement="top"
+              overlay={
+                <Tooltip>
+                  Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                  amet, consectetur, adipisci velit..." "There is no one who
+                  loves pain itself, who seeks after it and wants to have it,
+                  simply because it is pain...
+                </Tooltip>
+              }
+            >
+              <InfoCircle style={{ marginLeft: 10 }} />
+            </OverlayTrigger>
+          </div>
+          <AppRow>
+            <div>
+              <CurrencyDollar style={{ fontSize: "1.5rem" }} />
+            </div>
+            <div>
+              <div className={styles.totalValue}>
+                <span style={{}}>
+                  <span>Total</span>
+
+                  <span className={styles.gradientText}>&nbsp;64.4 USDT</span>
+                </span>
+                <OverlayTrigger
+                  placement="top"
+                  overlay={
+                    <Tooltip>
+                      Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                      amet, consectetur, adipisci velit..." "There is no one who
+                      loves pain itself, who seeks after it and wants to have
+                      it, simply because it is pain...
+                    </Tooltip>
+                  }
+                >
+                  <InfoCircle style={{ marginLeft: 5 }} />
+                </OverlayTrigger>
+              </div>
+              <div className={styles.subDataRow}>
+                <div>Price per tile</div>
+                <div> 0.1 USDT</div>
+              </div>
+            </div>
+          </AppRow>
+          <AppRow>
+            <InfoCircle />
+
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div className={styles.subDataRow}>
+                <div>Current owner:</div>
+
+                <div> zsef</div>
+              </div>
+              <div className={styles.subDataRow}>
+                <div>Purchased for:</div>
+                <div> 66.4 USDT</div>
+              </div>
+
+              <div className={styles.subDataRow}>
+                <div>Price per tile</div>
+                <div> 0.1 USDT</div>
+              </div>
+            </div>
+          </AppRow>
+
+          <AppRow>
+            <InfoCircle />
+            <div>
+              <div> Budapest, Szent Gyorgy Utca 2, 1013, Hungary</div>
+              <div className="text-secondary">19.03921 47.495895</div>
+            </div>
+          </AppRow>
+        </div>
+        <div
+          className={styles.tilesInfoCardBottom}
+          style={{ background: "#240051" }}
+        >
+          <div className={styles.moreLandInfo}>
+            <div>Current market value</div>
+            <div>721.79 USDT</div>
+            <span>10,20 %</span>
+          </div>
+          <div className={styles.moreLandInfo}>
+            <div>Original new land value:</div>
+            <div>0.1 USDT</div>
+            <div></div>
+          </div>
+          <div className={styles.tilesSelectedButtons}>
+            <Button
+              style={{
+                background:
+                  "linear-gradient(160.71deg,rgba(81,238,144,.2) .39%,rgba(17,17,17,0) 87.04%),radial-gradient(100% 100% at 100% 0%,#6100FF 0%,rgba(17,17,17,0) 100%),#874BE8",
+              }}
+            >
+              History
+            </Button>
+            <Button
+              style={{
+                background:
+                  "radial-gradient(100% 683.38% at 100% -1.52%,#AA20EB 0%,#FF6B00 100%)",
+              }}
+            >
+              Inspect
+            </Button>
+            <Button
+              style={{
+                background:
+                  "radial-gradient(100% 683.38% at 100% -1.52%,#AA20EB 0%,#FF6B00 100%)",
+              }}
+            >
+              Place an offer
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Col>
+  );
+}
+
+function AppRow({ children }) {
+  return (
+    <div className={styles.dataRow}>
+      <div className={styles.dataRowIcon}>{children[0]}</div>
+      <div style={{ flexGrow: "1" }}>{children[1]}</div>
+    </div>
+  );
+}
